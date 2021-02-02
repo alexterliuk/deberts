1e7bea6d96648447d8fa9f46015c428c9f8c826c
Alex Terliuk <alex.terliuk@gmail.com>
2021-02-02 21:00:52 +0200

    feat: clear path when redirected to Home from not existent page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export const Home: React.FC = () => {
+export const Home = () => {
+  const { location, replace } = useHistory();
+  if (location.pathname !== '/') {
+    replace('/');
+  }
+
   return (
     <>
       <h1>Deberts home page</h1>
