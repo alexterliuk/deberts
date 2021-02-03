337dd819071cb18ff54ea3d4b89c4c2d8db646fd
Alex Terliuk <alex.terliuk@gmail.com>
2021-02-03 22:14:30 +0200

    feat: add LangContext and temp dummy translations

---
+++ src/features/LangContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, ReactNode, useState } from 'react';
+import translations from '../translations';
+
+export type Language = 'UA' | 'RU' | 'EN';
+export type TranslationKey = keyof typeof translations.UA;
+
+const langStorage = {
+  get() {
+    return window.localStorage.getItem('lang') as Language;
+  },
+  set(lang: Language) {
+    window.localStorage.setItem('lang', lang);
+    return lang;
+  },
+};
+
+const LangContext = createContext<{
+  lang: Language;
+  changeLang: (newLang: Language) => void;
+  translations: typeof translations.UA;
+}>({
+  lang: 'UA',
+  changeLang: () => {},
+  translations: translations.UA,
+});
+
+export const LangProvider = ({ children }: { children: ReactNode }) => {
+  const storedLang = langStorage.get();
+  const [lang, setLang] = useState<Language>(storedLang || 'UA');
+
+  const provider = {
+    lang,
+    translations: translations[lang],
+    changeLang: (newLang: Language) => {
+      langStorage.set(newLang);
+      setLang(newLang);
+    },
+  };
+
+  return (
+    <LangContext.Provider value={provider}>{children}</LangContext.Provider>
+  );
+};
+
+export default LangContext;
---
+++ src/translations/index.ts
@@ -0,0 +1,11 @@
+import UA from './UA.json';
+import RU from './RU.json';
+import EN from './EN.json';
+
+const translations = {
+  UA,
+  RU,
+  EN,
+};
+
+export default translations;
